962e0eb0009dd033adc3ed1b3022a84554a8fb85
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-07-14 18:07:49 +0900

    d

--- src/mods/memory/index.ts
+++ src/mods/memory/index.ts
@@ -1,16 +1,17 @@
+import { Lengthed } from "@hazae41/lengthed"
 
-export interface Memory {
+export interface Memory<N extends number = number> {
 
   [Symbol.dispose](): void
 
-  readonly bytes: Uint8Array
+  readonly bytes: Uint8Array & Lengthed<N>
 
 }
 
-export class Slice {
+export class Slice<N extends number = number> implements Memory<N> {
 
   constructor(
-    readonly bytes: Uint8Array
+    readonly bytes: Uint8Array & Lengthed<N>
   ) { }
 
   [Symbol.dispose]() { }
